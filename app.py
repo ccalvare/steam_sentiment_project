ec97828bdd4165760ec02964c023ca6ab31e5e21
Todd Tuchek <toddtuchek@gmail.com>
2021-03-15 19:32:14 -0700

    main

---
+++ app.py
@@ -0,0 +1,17 @@
+# Import dependencies
+from flask import Flask
+import pandas as pd
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ predictor_api.py
@@ -0,0 +1,2 @@
+# This file contains functions to run the model
+
